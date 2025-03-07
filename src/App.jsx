75e2e2c86bd9e63af4cb8eaa1eedce01b3d47bf1
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-03-07 13:06:06 +0100

    error handle

--- src/App.jsx
+++ src/App.jsx
@@ -32,29 +32,37 @@ function App() {
   const [suggestions, setSuggestions] = useState([]);
   const [prodSelected, setProdSelected] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSearch = useCallback(
     debounce((query) => {
+      setErrorMessage("");
+
       fetch(
         `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
       )
         .then((res) => res.json())
         .then((data) => {
           /* console.log("data", data) */
-          query ? setSuggestions(data) : setSuggestions([]);
+          setSuggestions(data);
         })
-        .catch((err) => console.error(err));
+        .catch((err) => {
+          console.error(err);
+          setErrorMessage("Non è stato possibile trovare prodotti");
+        });
     }, 500),
     []
   );
 
   useEffect(() => {
+    if (!search) return setSuggestions([]);
     handleSearch(search);
   }, [search]);
 
   const handleSelectProduct = (product) => {
     /* console.log("product", product); */
     setIsLoading(true);
+    setErrorMessage("");
 
     fetch(
       `https://boolean-spec-frontend.vercel.app/freetestapi/products/${product.id}`
@@ -65,7 +73,12 @@ function App() {
         const { image, name, description, price } = data;
         setProdSelected({ image, name, description, price });
       })
-      .catch((err) => console.error(err))
+      .catch((err) => {
+        console.error(err);
+        setErrorMessage(
+          "Non è stato possibile scegliere il prodotto selezionato"
+        );
+      })
       .finally(() => setIsLoading(false));
 
     setSuggestions([]);
@@ -75,8 +88,18 @@ function App() {
     <main>
       <h1>Campo di ricerca intelligente</h1>
       <section>
+        {errorMessage && (
+          <div className="error-message-container">
+            <p className="error-message">{errorMessage}</p>
+          </div>
+        )}
+
         <div id="searchbar">
-          <input type="text" onChange={(e) => setSearch(e.target.value)} />
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Cerca un prodotto..."
+          />
           <div className="suggestions-list">
             {suggestions &&
               suggestions.map((s, i) => (
