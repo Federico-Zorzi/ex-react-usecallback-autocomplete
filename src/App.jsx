fd79c28d2651fe8298a1ccde2358d0a9886b9da8
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-03-07 10:41:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <h1>Campo di ricerca intelligente</h1>
+      <section>
+        <input type="text" />
+      </section>
+    </main>
+  );
+}
+
+export default App;
