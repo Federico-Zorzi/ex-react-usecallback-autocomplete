f2c779ca651ec20bd5eb33b8f7744c830a82b6ba
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-03-07 11:11:48 +0100

    call API and suggestions visualization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
+  const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const fetchSearch = (query) => {
+    fetch(
+      `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        /* console.log("data", data) */
+        query ? setSuggestions(data) : setSuggestions([]);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchSearch(search);
+  }, [search]);
+
   return (
     <main>
       <h1>Campo di ricerca intelligente</h1>
       <section>
-        <input type="text" />
+        <div id="searchbar">
+          <input type="text" onChange={(e) => setSearch(e.target.value)} />
+          {suggestions &&
+            suggestions.map((s, i) => (
+              <div key={i}>
+                <button className="suggestion-btn">{s.name}</button>
+              </div>
+            ))}
+        </div>
       </section>
     </main>
   );
